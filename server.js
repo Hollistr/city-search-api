1c3d7e3aba4906a5f808a9948b30d6e39a091223
Chris Hollis <chrisbhollis@gmail.com>
2022-10-04 15:49:08 -0700

    added modules movies.js and weather.js

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const express = require('express');
 // allows for Cross Origin Resource Sharing
 const cors = require('cors');
 
+const axios = require('axios');
+
+const { response } = require('express');
+
+// import modules
+const getMovies = require('./modules/movies.js');
+const getWeather = require('./modules/weather');
 // load data
 const data = require('./data/data.json');
 
@@ -25,64 +32,70 @@ app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
 
 // endpoint
 app.get('/weather', searchWeather);
+app.get('/movies', searchMovies);
 
-async function searchWeather(req, res) {
-    const url = process.env.REACT_APP_API_WEATHER_URL;
+// catch all endpoint
+app.get('/', (req, res) => {
+  res.status(404).send('Page Not Found');
+})
 
-    try {
-        const foundData = await axios.get(url, {
-            parameters: {
-                key: process.env.WEATHER_API_KEY,
-                lat: req.query.lat,
-                lon: req.query.lon,
-            },
-        });
-        res.status(200).send(ParseData(foundData.data, 'weather'));
-    } catch (error) {
-        res
-        .status(500)
-        .send(`Error occured on server: ${error.code} -${error.message}`);
-    }
-}
+// async function searchWeather(req, res) {
+//     const url = process.env.REACT_APP_API_WEATHER_URL;
 
-async function getMovies(req, res) {
-    const url = process.env.REACT_APP_API_MOVIES_URL;
+//     try {
+//         const foundData = await axios.get(url, {
+//             parameters: {
+//                 key: process.env.WEATHER_API_KEY,
+//                 lat: req.query.lat,
+//                 lon: req.query.lon,
+//             },
+//         });
+//         res.status(200).send(ParseData(foundData.data, 'weather'));
+//     } catch (error) {
+//         res
+//         .status(500)
+//         .send(`Error occured on server: ${error.code} -${error.message}`);
+//     }
+// }
+
+// async function searchMovies(req, res) {
+//     const url = process.env.REACT_APP_API_MOVIES_URL;
   
-    try {
-      const foundData = await axios.get(url, {
-        params: {
-          api_key: process.env.MOVIE_API_KEY,
-          query: req.query.searchQuery,
-        },
-      });
-      res.status(200).send(ParseData(foundData.data));
-    } catch (error) {
-      console.log(`Query failed! ${error.message}`);
-      res
-        .status(500)
-        .send(`Error occurred on server: ${error.code} - ${error.message}`);
-    }
-  }
+//     try {
+//       const foundData = await axios.get(url, {
+//         params: {
+//           api_key: process.env.MOVIE_API_KEY,
+//           query: req.query.searchQuery,
+//         },
+//       });
+//       res.status(200).send(ParseData(foundData.data));
+//     } catch (error) {
+//       console.log(`Query failed! ${error.message}`);
+//       res
+//         .status(500)
+//         .send(`Error occurred on server: ${error.code} - ${error.message}`);
+//     }
+//   }
 
-  class Forecast {
-    constructor(date, description) {
-        this.date = date;
-        this.description = description;
-    }
-  }
+//   class Forecast {
+//     constructor(date, description) {
+//         this.date = date;
+//         this.description = description;
+//     }
+//   }
 
-  class Movies {
-    constructor(obj) {
-      (this.title = obj.title),
-        (this.overview = obj.overview),
-        (this.average_votes = obj.average_votes),
-        (this.total_votes = obj.vote_count),
-        (this.image_url =
-          'https://image.tmdb.org/t/p/original' + obj.poster_path),
-        (this.popularity = obj.popularity),
-        (this.released_on = obj.release_date);
-    }
-  }
+//   class Movies {
+//     constructor(obj) {
+//       (this.title = obj.title),
+//         (this.overview = obj.overview),
+//         (this.average_votes = obj.average_votes),
+//         (this.total_votes = obj.vote_count),
+//         (this.image_url =
+//           'https://image.tmdb.org/t/p/original' + obj.poster_path),
+//         (this.popularity = obj.popularity),
+//         (this.released_on = obj.release_date);
+//     }
+//   }
   
 // catch all endpoint
 app.get('/', (req, res) => {
